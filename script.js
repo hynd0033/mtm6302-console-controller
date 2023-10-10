8f9f61921f3b84d8a0d5f2485b514fca18beb555
Conner <hynd0033@algonquinlive.com>
2023-10-10 19:01:08 -0400

    adds more

---
+++ script.js
@@ -0,0 +1,44 @@
+/* setTitle()
+Used to update the text of the <h1> element with the provided string
+
+setDescription()
+Used to update the text of the <p> element with the provided string
+
+setBackgroundColor()
+Used to update the background color of the <body> element with the provided string
+
+setFontColor()
+Used to update the font color for all elements with the provided string
+
+setTheme()
+Used to change the page theme to the theme provided. */
+
+// setTitle = "in the console?"
+
+
+const $setTitle = document.getElementById("title").innerHTML = "gfsgsdgssdg";
+
+const $setDescription = document.getElementById("description").innerHTML = "ddddd";
+
+const setBackgroundColor = document.querySelector('.background');
+
+setBackgroundColor.setAttribute('style', `
+background-color: red;
+`)
+
+const setFontColor = document.querySelector('.background');
+
+setFontColor.setAttribute('style', `
+color: yellow;
+`)
+
+
+
+
+
+
+
+
+// function myFunction
+
+// const setTitle = document.getElementById("title").innerHTML = "The next title!";
